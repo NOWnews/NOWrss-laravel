501925363d58410ac2533724cae182fc082595e8
root <root@nownews-cronjob-instance-20180726.c.nownews-website-199408.internal>
2018-09-26 10:13:12 +0000

    Add hinet layout.

---
+++ resources/views/hinet.blade.php
@@ -0,0 +1,30 @@
+@extends('base.base')
+
+@section('block')
+@foreach ($rssPosts as $rssPost)
+        <item>
+            <id>{{ $rssPost->ID }}</id>
+            <title><![CDATA[{{ $rssPost->title }}]]></title>
+            <link>{{ $rssPost->guid }}</link>
+            <description>
+                <![CDATA[
+                        @if ($rssPost->image)
+                        <div class="main-photo"><img src="{!! $rssPost->image->guid !!}" alt="{!! $rssPost->image->title !!}" /><cite>{!! $rssPost->image->content !!}</cite></div>
+                        @endif
+                        {!! $rssPost->content !!}
+                ]]>
+            </description>
+            <summary>
+                        {{ $rssPost->expert }}
+            </summary>
+            <guid isPermaLink="true">{{ $rssPost->guid }}</guid>
+            <subcategory>
+                <![CDATA[
+                    {{ $rssPost->subcategory }}
+                ]]>
+            </subcategory>
+            <author>{{ $rssPost->author }}</author>
+            <pubDate>{{ $rssPost->date }}</pubDate>
+        </item>
+@endforeach
+@endsection
