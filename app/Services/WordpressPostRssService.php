364bc138638ce087c2fcac8f06143a51ffab5c91
Div <b510336@gmail.com>
2019-11-27 01:58:20 +0800

    html_entity_decode content

--- app/Services/WordpressPostRssService.php
+++ app/Services/WordpressPostRssService.php
@@ -232,6 +232,9 @@ class WordpressPostRssService
 
     protected function replaceInvalidChar(string $string): string
     {
-        return preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', '', $string);
+        $string = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', '', $string);
+        $string = html_entity_decode($string);
+
+        return $string;
     }
 }
